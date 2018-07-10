43779c2130c9d219c1c2b9a06363ff49e735ef45
Bryan Delgado <obelisco456_@hotmail.com>
2018-07-10 16:23:03 -0500

    cambios

--- src/app/challenges/challenges.module.ts
+++ src/app/challenges/challenges.module.ts
@@ -4,11 +4,71 @@ import { CommonModule } from '@angular/common';
 import { ChallengesRoutingModule } from './challenges-routing.module';
 import { RankingComponent } from './ranking/ranking.component';
 import { ActivitiesComponent } from './activities/activities.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {
+  MatMenuModule, MatButtonToggleModule,
+  MatProgressSpinnerModule, MatSelectModule, MatSlideToggleModule, MatDialogModule, MatSnackBarModule,
+  MatTabsModule, MatSidenavModule, MatTooltipModule, MatRippleModule, MatRadioModule, MatGridListModule,
+  MatDatepickerModule, MatNativeDateModule, MatSliderModule, MatAutocompleteModule, MatCheckboxModule, MatProgressBarModule, MatAccordion, MatExpansionModule,
+} from '@angular/material';
 
+import { CovalentCommonModule } from '@covalent/core/common';
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentMediaModule } from '@covalent/core/media';
+import { CovalentLoadingModule } from '@covalent/core/loading';
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
+
+import { environment } from '../../environments/environment';
+import { AngularFireModule } from "angularfire2";
+
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 @NgModule({
   imports: [
     CommonModule,
-    ChallengesRoutingModule
+    ChallengesRoutingModule,
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatProgressSpinnerModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatTooltipModule,
+    MatRippleModule,
+    MatRadioModule,
+    MatGridListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatCheckboxModule,
+    MatProgressBarModule,
+    // covalent modules
+    CovalentCommonModule,
+    CovalentLayoutModule,
+    CovalentMediaModule,
+    CovalentLoadingModule,
+    //firebase
+    AngularFireModule.initializeApp(environment.config),
+    AngularFireAuthModule,
+    // external modules
+    NgxChartsModule,
   ],
   declarations: [RankingComponent, ActivitiesComponent]
 })
