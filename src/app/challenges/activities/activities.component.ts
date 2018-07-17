4313b1bca3506cd32ea58c752fd64e4b68c735d2
Bryan Delgado <obelisco456_@hotmail.com>
2018-07-17 11:26:48 -0500

    cambios

--- src/app/challenges/activities/activities.component.ts
+++ src/app/challenges/activities/activities.component.ts
@@ -4,11 +4,11 @@ import { ITdDataTableColumn } from '@covalent/core/data-table';
 import { TdDialogService } from '@covalent/core/dialogs';
 import { userDto } from '../../login/models/userDto';
 import { LoginService } from '../../login/service/login.service';
-import { Router,ActivatedRoute, Params, NavigationExtras } from '@angular/router';
+import { Router, ActivatedRoute, Params, NavigationExtras } from '@angular/router';
 import { uidDto } from '../../login/models/uidDto';
 import { ViewContainerRef } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import {LoginComponent} from '../../login/login/login.component'
+import { LoginComponent } from '../../login/login/login.component'
 import { ChallengesService } from '../services/challenges.service'
 import { activityDto } from '../models/activityDto';
 import { all } from '../../../../node_modules/@types/q';
@@ -26,32 +26,29 @@ export class ActivitiesComponent implements OnInit {
   uidDto: uidDto;
   disableClose: boolean;
   allActivity: activityDto[];
-  activity:activityDto;
+  activity: activityDto;
 
-  
-  constructor(private _router: Router, private loginSVC: LoginService, 
-     _dialogService: TdDialogService, private _viewContainerRef: ViewContainerRef, 
-     private challengesSVC: ChallengesService,private _dialogRef: MatDialog, private _loadingService: TdLoadingService) {
+  constructor(private _router: Router, private loginSVC: LoginService,
+    _dialogService: TdDialogService, private _viewContainerRef: ViewContainerRef,
+    private challengesSVC: ChallengesService, private _dialogRef: MatDialog, private _loadingService: TdLoadingService) {
     this.userDto = new userDto();
     this.uidDto = new uidDto();
-    this.allActivity= new Array<activityDto>();
-    
+    this.allActivity = new Array<activityDto>();
   }
-  openConfirm(dato:activityDto): void {
-  console.log(dato.id);
-  localStorage.setItem('idActivity', ""+dato.id)
+  openConfirm(dato: activityDto): void {
+    console.log(dato.id);
+    localStorage.setItem('idActivity', "" + dato.id)
     const dialogRef = this._dialogRef.open(DetailActivitiesComponent, {
       width: '500px',
-      data: {data: dato}
+      data: { data: dato }
     });
 
     dialogRef.afterClosed().subscribe(result => {
       localStorage.removeItem('idActivity');
       console.log('The dialog was closed', result);
-      
     });
   }
-  pasarDatos(activity:activityDto){
+  pasarDatos(activity: activityDto) {
     let NavigationExtras: NavigationExtras = {
       queryParams: {
         "activities": JSON.stringify(activity),
@@ -59,51 +56,43 @@ export class ActivitiesComponent implements OnInit {
     };
     this._router.navigate(["ranking"], NavigationExtras);
   }
-    /**
-    this._dialogService.openConfirm({
-      message: 'ingresar a la actividad',
-      disableClose: true , // defaults to false
-      viewContainerRef: this._viewContainerRef, //OPTIONAL
-      title: 'Actividad:', //OPTIONAL, hides if not provided
-      //cancelButton: '', //OPTIONAL, defaults to 'CANCEL'
-      acceptButton: 'Ingresar a la actividad', //OPTIONAL, defaults to 'ACCEPT'
-      width: '500px', //OPTIONAL, defaults to 400px
-    }).afterClosed().subscribe((accept: boolean) => {
-      if (accept) {
-        // DO SOMETHING
-        this._router.navigate(['login']);
-      } else {
-        // DO SOMETHING ELSE
-      }
-    });
-     */
-  
-
+  /**
+  this._dialogService.openConfirm({
+    message: 'ingresar a la actividad',
+    disableClose: true , // defaults to false
+    viewContainerRef: this._viewContainerRef, //OPTIONAL
+    title: 'Actividad:', //OPTIONAL, hides if not provided
+    //cancelButton: '', //OPTIONAL, defaults to 'CANCEL'
+    acceptButton: 'Ingresar a la actividad', //OPTIONAL, defaults to 'ACCEPT'
+    width: '500px', //OPTIONAL, defaults to 400px
+  }).afterClosed().subscribe((accept: boolean) => {
+    if (accept) {
+      // DO SOMETHING
+      this._router.navigate(['login']);
+    } else {
+      // DO SOMETHING ELSE
+    }
+  });
+   */
   getUserData() {
     this._loadingService.register();
     this.uidDto.id = localStorage.getItem('uid');
     this.loginSVC.getUserData(this.uidDto).then(res => {
       this.userDto = res;
       this._loadingService.resolve();
-
       console.log(this.userDto.name, this.userDto.lastname, this.userDto.experience);
     })
   }
-
-
   ngOnInit() {
-    
     this.getUserData();
     this.getAllActivity()
-
   }
   public getAllActivity() {
     this._loadingService.register();
     this.challengesSVC.getAllActivy().then(res => {
-     this.allActivity= res;
-     this._loadingService.resolve();
-     console.log(this.allActivity)
+      this.allActivity = res;
+      this._loadingService.resolve();
+      console.log(this.allActivity)
     })
   }
-
 }
