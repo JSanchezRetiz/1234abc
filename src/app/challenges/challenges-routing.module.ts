dba9227e94d3eca237a1074ceb799b9a9b7db31c
Bryan Delgado <obelisco456_@hotmail.com>
2018-07-26 10:04:41 -0500

    cambios

--- src/app/challenges/challenges-routing.module.ts
+++ src/app/challenges/challenges-routing.module.ts
@@ -6,6 +6,7 @@ import { StoreComponent } from './store/store.component';
 import { DetailActivitiesComponent } from './detail-activities/detail-activities.component';
 import { DetailArticleComponent } from './detail-article/detail-article.component';
 import { CreateActivityComponent } from './create-activity/create-activity.component';
+import {MainComponent} from  '../main.component';
  
 const routes: Routes = [
   { path:'ranking', component:RankingComponent},
@@ -14,7 +15,7 @@ const routes: Routes = [
   { path:'detalles-actividades', component:DetailActivitiesComponent},
   { path:'detalles-articulo', component:DetailArticleComponent},
   { path:'crear-actividad', component:CreateActivityComponent},
-
+  { path:'', component:MainComponent},
 ];
 
 @NgModule({
