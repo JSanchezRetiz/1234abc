fa8af7211bd5afae78d838b429f57c18363267b3
Bryan Delgado <obelisco456_@hotmail.com>
2018-08-23 16:41:31 -0500

    cambios

--- src/app/coordinator/users/users.component.ts
+++ src/app/coordinator/users/users.component.ts
@@ -5,34 +5,40 @@ import { TdDialogService } from '@covalent/core/dialogs';
 import { Router } from '@angular/router';
 import { CoordinatorService } from '../services/coordinator.service';
 import { storage } from '../../../../node_modules/firebase';
-import {usersDto} from '../models/usersDto';
+import { usersDto } from '../models/usersDto';
 import { userDto } from '../../login/models/userDto';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
-  providers: [TdLoadingService,CoordinatorService]
+  providers: [TdLoadingService, CoordinatorService]
 })
 export class UsersComponent implements OnInit {
-users: userDto[];
+  users: userDto[];
+  usersSend: usersDto;
+  quantity: number;
 
-  constructor(private coordinatorSVC: CoordinatorService,  private _loadingService: TdLoadingService,  private _router: Router, private _dialogService: TdDialogService, private _viewContainerRef: ViewContainerRef, ) { 
- 
+  constructor(private coordinatorSVC: CoordinatorService, private _loadingService: TdLoadingService, private _router: Router, private _dialogService: TdDialogService, private _viewContainerRef: ViewContainerRef, ) {
+    this.usersSend = new usersDto();
   }
 
-  getAllUsers(){
-    this.coordinatorSVC.getAllUsers().then(res=>{
-      this.users=res;
+  getAllUsers() {
+    this.coordinatorSVC.getAllUsers().then(res => {
+      this.users = res;
       console.log(res);
+      console.log('cantidad', this.users.length)
+      console.log('cantidad', this.usersSend.idUser.length)
     })
   }
 
-  createUser(){
+  createUser() {
     this._router.navigate(["registro"]);
   }
 
   ngOnInit() {
     this.getAllUsers();
+  
+
   }
 
 }
